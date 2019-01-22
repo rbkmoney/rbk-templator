c019b2d6f05f7a19297d59bd8a294bc00542711d
n.pospolita <n.pospolita@rbkmoney.com>
2019-01-22 13:56:34 +0300

    fix .xml

--- generators/proto-generator.js
+++ generators/proto-generator.js
@@ -57,7 +57,7 @@ module.exports = {
         {
             type: 'add',
             path: '{{pathHelper path}}pom.xml',
-            templateFile: 'plop-templates/proto/proto-pom'
+            templateFile: 'plop-templates/proto/proto-pom.xml'
         }
     ]
 };
