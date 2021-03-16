70f92c8b4330753349f648a73843fcde30bdc598
NPospolita <nikita7asics@gmail.com>
2021-03-16 17:39:14 +0300

    fixed imports and compile errors

--- plop-templates/adapter/payout/src/client/converter/RequestConverter.java
+++ plop-templates/adapter/payout/src/client/converter/RequestConverter.java
@@ -1,6 +1,6 @@
 package com.rbkmoney.{{adapterPayoutPackageCase bank_name}}.client.converter;
 
-import com.rbkmoney.adapter.payout.kekespekes.client.model.BaseRequest;
+import com.rbkmoney.{{adapterPayoutPackageCase bank_name}}.client.model.BaseRequest;
 
 /**
  * Родительский класс для конвертации запросов к третьей стороне
--- plop-templates/adapter/payout/src/config/ProcessorConfig.java
+++ plop-templates/adapter/payout/src/config/ProcessorConfig.java
@@ -1,6 +1,11 @@
 package com.rbkmoney.{{adapterPayoutPackageCase bank_name}}.config;
 
 import com.rbkmoney.adapter.bank.payout.spring.boot.starter.processor.Processor;
+import com.rbkmoney.{{adapterPayoutPackageCase bank_name}}.client.model.BaseResponse;
+import com.rbkmoney.{{adapterPayoutPackageCase bank_name}}.model.EntryStateModelImpl;
+import com.rbkmoney.{{adapterPayoutPackageCase bank_name}}.model.ExitStateModelImpl;
+import com.rbkmoney.{{adapterPayoutPackageCase bank_name}}.processor.ErrorProcessor;
+import com.rbkmoney.{{adapterPayoutPackageCase bank_name}}.processor.SuccessProcessor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
