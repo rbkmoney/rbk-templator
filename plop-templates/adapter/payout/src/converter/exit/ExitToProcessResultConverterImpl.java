1dc41e142730400771c9c9f7b22c8f0a1c800a2c
vitaxa <vitaxa93gamebox@gmail.com>
2021-10-27 15:56:19 +0300

    fix ci test
    fix java template

--- plop-templates/adapter/payout/src/converter/exit/ExitToProcessResultConverterImpl.java
+++ plop-templates/adapter/payout/src/converter/exit/ExitToProcessResultConverterImpl.java
@@ -92,6 +92,7 @@ public class ExitToProcessResultConverterImpl implements ExitStateToProcessResul
 //            default:
 //                throw new UnsupportedOperationException("Unknown step : " + step);
 //        }
+{{/if_eq}}
 
         final ProcessResult processResult = new ProcessResult();
         processResult.setNextState(Value.bin(stateSerializer.writeByte(nextState)));
